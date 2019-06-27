c5a5e430315dbb9c2028fd72c295ddbee4cbe128
MoeinShafienia <shafienia.moein@gmail.com>
2019-06-27 15:43:31 +0430

    src

---
+++ src/java/ir/ac/kntu/Main.java
@@ -0,0 +1,25 @@
+package ir.ac.kntu;
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Pane root = new Pane();
+        Scene scene = new Scene(root, 800, 800);
+
+        
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
